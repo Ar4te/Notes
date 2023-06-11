8ad8f044e214d89078bf53a4e2a4f84f84d009f5
Ar4te <1101458943@qq.com>
2023-06-11 23:48:18 +0800

    123

--- Notes/Views/LoginPage.xaml.cs
+++ Notes/Views/LoginPage.xaml.cs
@@ -18,12 +18,14 @@ public partial class LoginPage : ContentPage
         if (BindingContext is Login login && !string.IsNullOrEmpty(login.userNo) && !string.IsNullOrEmpty(login.password))
         {
             var res = await HttpHelper.GetAsync(ServerUrls.GetToken(login));
-            await DisplayAlert("登陆成功", $"欢迎您{login.userNo}", "OK");
+            //await DisplayAlert("登陆成功", $"欢迎您{login.userNo}", "OK");
+            await Toast.Make("登陆成功", ToastDuration.Short, 14).Show(new CancellationTokenRegistration().Token);
+            await Shell.Current.GoToAsync("..");
         }
         else
         {
             //DisplayAlert("登陆失败", $"请正确输入登录名及密码", "OK");
-            await Toast.Make("请正确输入登录名及密码", ToastDuration.Short, 14).Show((new CancellationTokenSource()).Token);
+            await Toast.Make("请正确输入登录名及密码", ToastDuration.Short, 14).Show(new CancellationTokenSource().Token);
         }
     }
 }
