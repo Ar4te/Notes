04321380d790a8df618067be4be8ed539f030868
wangyingbin <yingbin.wang@superstar-auto.com>
2023-06-16 21:00:08 +0800

    123

--- Notes/Views/UserInfoPage.xaml.cs
+++ Notes/Views/UserInfoPage.xaml.cs
@@ -2,8 +2,12 @@ namespace Notes.Views;
 
 public partial class UserInfoPage : ContentPage
 {
-	public UserInfoPage()
-	{
-		InitializeComponent();
-	}
+    public UserInfoPage()
+    {
+        InitializeComponent();
+        //webView.Source = @"http://121.196.199.65:18882/swagger/index.html";
+        webView.Source = @"https://www.csdn.net";
+        webView.HeightRequest = DeviceDisplay.Current.MainDisplayInfo.Height / 3;
+        webView.WidthRequest = DeviceDisplay.Current.MainDisplayInfo.Width / 3;
+    }
 }
